897aa92b5cfe28c760d67987509e72324f63ccf9
Sezoka <vlad.novport@gmail.com>
2023-06-14 18:39:12 +0300

    add minus

--- src/evaluator.zig
+++ src/evaluator.zig
@@ -19,6 +19,8 @@ pub fn eval(node: ast.Node) object.Object {
 fn eval_prefix_expression(operator: []const u8, right: object.Object) object.Object {
     if (std.mem.eql(u8, operator, "!")) {
         return eval_bang_operator_expression(right);
+    } else if (std.mem.eql(u8, operator, "-")) {
+        return eval_minus_prefix_operator_expression(right);
     } else {
         return .{ .null_ = .{} };
     }
@@ -30,6 +32,15 @@ fn eval_bang_operator_expression(right: object.Object) object.Object {
         .null_ => .{ .boolean = .{ .value = true } },
         else => .{ .boolean = .{ .value = false } },
     };
+}
+
+fn eval_minus_prefix_operator_expression(right: object.Object) object.Object {
+    if (object.kind(right) != .integer) {
+        return .{ .null_ = .{} };
+    }
+
+    const negated = -right.integer.value;
+    return .{ .integer = .{ .value = negated } };
 }
 
 pub fn eval_statements(statements: []ast.Statement) object.Object {
