1e01fdd1463509695239df9ef61c9aceaf901756
Sezoka <vlad.novport@gmail.com>
2023-06-14 18:31:30 +0300

    bang evaluator

--- src/evaluator.zig
+++ src/evaluator.zig
@@ -1,3 +1,4 @@
+const std = @import("std");
 const ast = @import("ast.zig");
 const object = @import("object.zig");
 
@@ -6,7 +7,28 @@ pub fn eval(node: ast.Node) object.Object {
         .program => |p| eval_statements(p.statements),
         .expr_stmt => |es| eval(ast.expression_to_node(es.expression.?)),
         .int => |i| .{ .integer = .{ .value = i.value } },
+        .boolean => |b| .{ .boolean = .{ .value = b.value } },
+        .prefix => |p| {
+            const right = eval(ast.expression_to_node(p.right));
+            return eval_prefix_expression(p.operator, right);
+        },
         else => .{ .null_ = .{} },
+    };
+}
+
+fn eval_prefix_expression(operator: []const u8, right: object.Object) object.Object {
+    if (std.mem.eql(u8, operator, "!")) {
+        return eval_bang_operator_expression(right);
+    } else {
+        return .{ .null_ = .{} };
+    }
+}
+
+fn eval_bang_operator_expression(right: object.Object) object.Object {
+    return switch (right) {
+        .boolean => |b| .{ .boolean = .{ .value = !b.value } },
+        .null_ => .{ .boolean = .{ .value = true } },
+        else => .{ .boolean = .{ .value = false } },
     };
 }
 
